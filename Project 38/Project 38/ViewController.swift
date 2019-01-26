816687c4b4ecf19d1d6a1d04207548d15288c3c8
MrTeller <pav.tolmach@gmail.com>
2019-01-26 21:47:06 +0300

    Loading from JSON and saving to CoreData - done

--- Project 38/Project 38/ViewController.swift
+++ Project 38/Project 38/ViewController.swift
@@ -21,10 +21,38 @@ class ViewController: UITableViewController {
                 print("Inresolved error \(error.localizedDescription)")
             }
         }
-        let commit = Commit()
-        commit.message = "woo"
-        commit.url = "http://www.example.com"
-        commit.date = Date()
+//        let commit = Commit()
+//        commit.message = "woo"
+//        commit.url = "http://www.example.com"
+//        commit.date = Date()
+        performSelector(inBackground: #selector(fetchCommits), with: nil) //Paul's note: compare with othe background fetching technicues
+    }
+    
+    @objc func fetchCommits() { //Paul's note: make real error handling someday
+        if let data = try? String(contentsOf: URL(string: "https://api.github.com/repos/apple/swift/commits?per_page=100")!) {
+            // give the data to SwiftlyJSON to parse
+            let jsonCommits = JSON(parseJSON: data)
+            // read the commits back out
+            let jsonCommitArray = jsonCommits.arrayValue
+            print("Received \(jsonCommitArray.count) new commits.")
+            
+            DispatchQueue.main.async { [unowned self] in
+                for jsonCommit in jsonCommitArray {
+                    let commit = Commit(context: self.container.viewContext)
+                    self.configure(commit: commit, usingJSON: jsonCommit)
+                }
+                self.saveContext() // Paul's note: that's right: viewContext is main thread context and should be saved (and operated) on main thread only. Even Asycncroniously.
+            }
+        }
+    }
+    
+    private func configure(commit: Commit, usingJSON json: JSON) {
+        commit.sha = json["sha"].stringValue
+        commit.message = json["commit"]["message"].stringValue
+        commit.url = json["html_url"].stringValue
+        
+        let formatter = ISO8601DateFormatter()
+        commit.date = formatter.date(from: json["commit"]["commiter"]["date"].stringValue) ?? Date()
     }
     
     private func saveContext() {
