4a4d5f01f3f972ffbe80716cdb4a86a665595468
LaconicTranslator <translator@laconic.website>
2019-02-11 12:15:55 +0300

    Applying AVAudioSession, AVAudioRecorder, AVAudioPlayer together. AVAudioSession - advanced recording parameters.

--- Project 33/Project 33/RecordWhistleViewController.swift
+++ Project 33/Project 33/RecordWhistleViewController.swift
@@ -9,16 +9,18 @@
 import UIKit
 import AVFoundation
 
-class RecordWhistleViewController: UIViewController {
+class RecordWhistleViewController: UIViewController, AVAudioRecorderDelegate {
     var stackView: UIStackView!
     var recordButton: UIButton!
+    var playButton: UIButton!
     
     var recordingSession: AVAudioSession!
     var whistleRecorder: AVAudioRecorder!
+    var whistlePlayer: AVAudioPlayer!
     
     override func loadView() {
         view = UIView()
-        view.backgroundColor = UIColor.gray
+        view.backgroundColor = UIColor.gray // Paul note: why grey? It is going to be red or green
         stackView = UIStackView()
         stackView.spacing = 30
         stackView.translatesAutoresizingMaskIntoConstraints = false // Autolayout dynamically calculates size
@@ -55,11 +57,130 @@ class RecordWhistleViewController: UIViewController {
     }
     
     private func loadRecordingUI() {
-        
+        recordButton = UIButton()
+        recordButton.translatesAutoresizingMaskIntoConstraints = false
+        recordButton.setTitle("Tap to record", for: .normal)
+        recordButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1) // Paul's note: Add notification to update.t
+        recordButton.addTarget(self, action: #selector(recordTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(recordButton)
+        playButton = UIButton()
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        playButton.setTitle("Tap to Play", for: .normal)
+        playButton.isHidden = true
+        playButton.alpha = 0
+        playButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+        playButton.addTarget(self, action: #selector(playTapped), for: .touchUpInside)
+        stackView.addArrangedSubview(playButton)
     }
     
     private func loadFailUI() {
+        let failLabel = UILabel()
+        failLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        failLabel.text = "Recording failed: please ensure the app has access to your microfone."
+        failLabel.numberOfLines = 0
+        stackView.addArrangedSubview(failLabel)
+    }
+    
+    class func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
+    class func getWhistleURL() -> URL {
+        return getDocumentsDirectory().appendingPathComponent("whistle.m4a")
+    }
+    
+    private func startRecording() {
+        // 1
+        view.backgroundColor = UIColor(displayP3Red: 0.6, green: 0, blue: 0, alpha: 1)
+        // 2
+        recordButton.setTitle("Tap to Stop", for: .normal)
+        // 3
+        let audioURL = RecordWhistleViewController.getWhistleURL()
+        print(audioURL.absoluteString)
+        // 4
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
         
+        do {
+            // 5
+            whistleRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
+            whistleRecorder.delegate = self
+            whistleRecorder.record()
+        } catch  {
+            finishRecording(success: false)
+        }
+    }
+    
+    private func finishRecording(success: Bool) {
+        view.backgroundColor = UIColor(red: 0, green: 0.6, blue: 0, alpha: 1)
+        #if DEBUG
+        print("\(Date()) \(#file)(\(#line)) \(#function):\r")
+        #endif
+        
+        whistleRecorder.stop()
+        whistleRecorder = nil
+        
+        if success {
+            recordButton.setTitle("Tap to Re-record", for: .normal)
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(nextTapped))
+            if playButton.isHidden {
+                UIView.animate(withDuration: 0.35) { [unowned self] in
+                    self.playButton.isHidden = false
+                    self.playButton.alpha = 1
+                }
+            }
+        } else {
+            recordButton.setTitle("Tap to Record", for: .normal)
+            let ac = UIAlertController(title: "Record failed", message: "There was a problem recording your whistle; please try again", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+        }
+    }
+    
+    @objc private func nextTapped() {
+
+    }
+    
+    @objc private func playTapped() {
+        let audioURL = RecordWhistleViewController.getWhistleURL() // Paul's note: if let wants to be here
+        
+        do {
+            whistlePlayer = try AVAudioPlayer(contentsOf: audioURL)
+            whistlePlayer.play()
+        } catch {
+            let ac = UIAlertController(title: "Playback failed", message: "There was a problem playing your whistle; please try re-recording", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+        }
+    }
+    
+    @objc func recordTapped() {
+        if whistleRecorder == nil {
+            startRecording()
+            
+            if !playButton.isHidden {
+                UIView.animate(withDuration: 0.35) { [unowned self] in
+                    self.playButton.isHidden = true
+                    self.playButton.alpha = 0
+                }
+            }
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    
+    // TODO: check to see if this is usefull
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: flag)
+        }
+        #if DEBUG
+        print("\(Date()) \(#file)(\(#line)) \(#function):\r")
+        #endif
     }
     /*
     // MARK: - Navigation
