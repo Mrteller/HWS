429907c81586e328d7348ba4161db3386f5a3607
LaconicTranslator <translator@laconic.website>
2019-02-25 20:34:04 +0300

    Reading from iCloud.

--- Project 33/Project 33/ViewController.swift
+++ Project 33/Project 33/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UITableViewController {
         title = "What's that Whistle?"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
         navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,12 +36,73 @@ class ViewController: UITableViewController {
     }
     
     func loadWhistles() {
+        // Unavalible iOS
+        // let metaQuery = "(kMDItemAuthors ==[c] 'Daniel' || kMDItemAuthors ==[c] 'Steve' ) && (kMDItemContentType == 'public.audio' || kMDItemContentType == 'public.video')"
+        // let pred = NSPredicate(fromMetadataQueryString: metaQuery)
+        let pred = NSPredicate(value: true)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: false) // use static lets
+        let query = CKQuery(recordType: "Whistles", predicate: pred)
+        query.sortDescriptors = [sort]
         
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["genre", "comments"]
+        
+        operation.resultsLimit = 50
+        // If we had a struct. Or if we had init for class.
+        // operation.recordFetchedBlock = { record in newWhistles.append(Whistles(record.object(forKey: "genre"), record.object(forKey: "comments"))}
+        
+        var newWhistles = [Whistle]()
+        operation.recordFetchedBlock = { record in
+            let whistle = Whistle()
+            whistle.recordID = record.recordID
+            whistle.genre = record["genre"]
+            whistle.comments = record["comments"]
+            newWhistles.append(whistle)
+        }
+        
+        operation.queryCompletionBlock = { [unowned self] (cursor, error) in
+            DispatchQueue.main.async {
+                if error == nil {
+                    ViewController.isDirty = false
+                    self.whistles = newWhistles
+                    self.tableView.reloadData()
+                } else {
+                    let ac = UIAlertController(title: "Fetch failed", message: "There was a problem fetching the list of whistles; please try again: \(error!.localizedDescription)", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(ac, animated: true)
+                }
+            }
+        }
+        
+        CKContainer.default().publicCloudDatabase.add(operation)
     }
 
     @objc func addWhistle() {
         let vc = RecordWhistleViewController()
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    private func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), .foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        let titleString = NSMutableAttributedString(string: "\(title)", attributes: titleAttributes)
+        
+        if subtitle.count > 0 {
+            let subtitleString = NSAttributedString(string: "\n\(subtitle)", attributes: subtitleAttributes)
+            titleString.append(subtitleString)
+        }
+        return titleString
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.attributedText = makeAttributedString(title: whistles[indexPath.row].genre, subtitle: whistles[indexPath.row].comments)
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.whistles.count
     }
 }
 
