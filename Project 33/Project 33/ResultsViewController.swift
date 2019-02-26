8f8708920d95862aa1fff9ff03d89ffd5ea3fb11
LaconicTranslator <translator@laconic.website>
2019-02-26 22:53:58 +0300

    Creating one to many relationship.

---
+++ Project 33/Project 33/ResultsViewController.swift
@@ -0,0 +1,159 @@
+//
+//  ResultsViewController.swift
+//  Project 33
+//
+//  Created by Paul on 25.02.2019.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import CloudKit
+
+class ResultsViewController: UITableViewController {
+    var whistle: Whistle!
+    var suggestions = [String]()
+    
+    var whistlePlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1 {
+            return "Suggested songs"
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return suggestions.count + 1
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.textLabel?.numberOfLines = 0
+        
+        if indexPath.section == 0 {
+            // the user's comments about this whistle
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .title1)
+            
+            if whistle.comments.count == 0 {
+                cell.textLabel?.text = "Comments: None"
+            } else {
+                cell.textLabel?.text = whistle.comments
+            }
+        } else {
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            if indexPath.row == suggestions.count {
+                // this is our extra row
+                cell.textLabel?.text = "Add suggestion"
+                cell.selectionStyle = .gray
+            } else {
+                cell.textLabel?.text = suggestions[indexPath.row]
+            }
+        }
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section == 1 && indexPath.row == suggestions.count else { return }
+        tableView.deselectRow(at: indexPath, animated: true)
+        let ac = UIAlertController(title: "suggest a song...", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Submit", style: .default) { [unowned self] action in
+            if let textField = ac.textFields?.first, (textField.text?.count ?? 1) > 0 {
+            self.add(suggestion: textField.text!)
+            }
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    private func add(suggestion: String) {
+        let whistleRecord = CKRecord(recordType: "Suggestions")
+        let reference = CKRecord.Reference(recordID: whistle.recordID, action: .deleteSelf)
+        whistleRecord["text"] = suggestion // as CKRecordValue
+        whistleRecord["owningWhistle"] = reference // as CKRecordValue
+        
+        CKContainer.default().publicCloudDatabase.save(whistleRecord) { [unowned self] record, error in
+            DispatchQueue.main.async {
+                if error == nil {
+                    self.suggestions.append(suggestion)
+                    self.tableView.reloadData() // or insert row
+                } else {
+                    let ac = UIAlertController(title: "Error", message: "There was a problem submitting your suggestion: \(error!.localizedDescription)", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(ac, animated: true)
+                }
+            }
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
